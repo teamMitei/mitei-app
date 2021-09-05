3478b8a09a22110559f186b7296f4e4198d7ddc6
SUZUNA-HASEGAWA <hasegawa.suzuna@g.wakayama-u.jp>
2021-09-05 22:08:42 +0900

    20210905

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
 import 'package:flutter/material.dart';
-// file
-import 'package:location_3/map.dart';
+import 'first_view.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyMap(title: '位置情報'),
-    );
-  }
-}
+void main() => runApp(FirstView());
